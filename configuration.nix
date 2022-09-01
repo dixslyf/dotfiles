a2925cb9c5ac2d0b25d1071f31af18be5d866943
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-01 22:52:40 +0800

    fix: uncomment env var that was accidentally commented out
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- configuration.nix
+++ configuration.nix
@@ -182,7 +182,7 @@
   environment.loginShellInit = ''
     export LIBVA_DRIVER_NAME=nvidia
     export CLUTTER_BACKEND=wayland
-    # export XDG_SESSION_TYPE=wayland
+    export XDG_SESSION_TYPE=wayland
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
     export MOZ_ENABLE_WAYLAND=1
     export GBM_BACKEND=nvidia-drm
