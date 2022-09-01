781fe649d3a4630b0b6698086fbf7cf66cd6abae
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-01 22:51:59 +0800

    refactor(nixos): switch to nixos-unstable
    Based on what others say online, nixos-unstable isn't actually that
    unstable and only breaks occassionally. Seems to fix xwayland on
    hyprland as well.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     }; 
     impermanence.url = "github:nix-community/impermanence";
@@ -12,7 +12,10 @@
       url = "github:fufexan/nix-gaming";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ { self, nixpkgs, home-manager, impermanence, nix-gaming, hyprland }: {
