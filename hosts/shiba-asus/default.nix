a6644ec9fab68675154decfb944795ef2040fc54
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-05 13:29:08 +0800

    fix(xorg): fix sddm not starting window manager by setting wayland variables in a wrapper script for hyprland
    Not sure if the issue is specific to sddm and bspwm.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- hosts/shiba-asus/default.nix
+++ hosts/shiba-asus/default.nix
@@ -102,20 +102,6 @@
     desktopManager.runXdgAutostartIfNone = true;
   };
 
-  environment.loginShellInit = ''
-    export LIBVA_DRIVER_NAME=nvidia
-    export CLUTTER_BACKEND=wayland
-    export XDG_SESSION_TYPE=wayland
-    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-    export MOZ_ENABLE_WAYLAND=1
-    export GBM_BACKEND=nvidia-drm
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export WLR_NO_HARDWARE_CURSORS=1
-    export WLR_BACKEND=vulkan
-    export QT_QPA_PLATFORM=wayland
-    export GDK_BACKEND=wayland
-  '';
-
   services = {
     earlyoom = {
       enable = true;
@@ -195,9 +181,23 @@
     gamemode = { enable = true; };
     hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.default.override {
-        nvidiaPatches = true;
-      };
+      package = (inputs.hyprland.packages.${pkgs.system}.default.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [ pkgs.makeWrapper ];
+	postInstall = oldAttrs.postInstall or ''
+          wrapProgram $out/bin/Hyprland \
+            --set LIBVA_DRIVER_NAME nvidia \
+            --set CLUTTER_BACKEND wayland \
+            --set XDG_SESSION_TYPE wayland \
+            --set QT_WAYLAND_DISABLE_WINDOWDECORATION 1 \
+            --set MOZ_ENABLE_WAYLAND 1 \
+            --set GBM_BACKEND nvidia-drm \
+            --set __GLX_VENDOR_LIBRARY_NAME nvidia \
+            --set WLR_NO_HARDWARE_CURSORS 1 \
+            --set WLR_BACKEND vulkan \
+            --set QT_QPA_PLATFORM wayland \
+            --set GDK_BACKEND wayland
+	'';
+      })).override { nvidiaPatches = true; };
     };
   };
 }
