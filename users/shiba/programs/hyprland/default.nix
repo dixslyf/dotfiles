8dd24b8e8d948ae577d5d27f6e96663734373506
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-14 12:52:52 +0800

    feat(hyprland): add workspace bindings for most keys on the keyboard
    Moving away from using numbers as workspaces as it is easy to mispress
    and move to the wrong workspace due to the fact that the number row is
    two rows away from the home row. Using other keys feels more ergonomic.
    The numbers are still usable as workspaces, but I'll likely rarely use
    them.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/hyprland/default.nix
+++ users/shiba/programs/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [ inputs.hyprland.homeManagerModules.default ];
@@ -8,6 +8,24 @@
     package = null;  # required to use the nixOS module to install hyprland
     xwayland.enable = true;
     # use extraConfig so that the hyprland flake still adds in the lines for systemd integration
-    extraConfig = builtins.readFile ./hyprland.conf;
+    extraConfig = let
+      config = pkgs.substituteAll {
+        src = ./hyprland.conf;
+        workspaceBindings = let
+          keys = [
+            "grave" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "minus" "equal"
+            "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "bracketleft" "bracketright" "backslash"
+            "a" "s" "d" "f" "g" "h" "j" "k" "l" "semicolon" "apostrophe"
+            "z" "x" "c" "v" "b" "n" "m" "comma" "period" "slash"
+          ];
+          in
+          "\n"+ builtins.concatStringsSep "\n" (builtins.map
+            (key: ''
+              bind = ,${key},workspace,name:${key}
+              bind = SHIFT,${key},movetoworkspacesilent,name:${key}
+            '')
+            keys);
+      };
+      in builtins.readFile "${config}";
   };
 }
