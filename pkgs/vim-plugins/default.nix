393f3dd47ea6c7d5525df040db2291ae25a397d2
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-19 22:50:13 +0800

    refactor(pvtpkgs): use buildVimPluginFrom2Nix for vim plugins
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- pkgs/vim-plugins/default.nix
+++ pkgs/vim-plugins/default.nix
@@ -1,39 +1,17 @@
 {
   inputs,
   vimUtils,
-}: {
-  catppuccin-nvim = vimUtils.buildVimPlugin {
-    pname = "catppuccin-nvim";
-    version = inputs.catppuccin-nvim.lastModifiedDate;
-    src = inputs.catppuccin-nvim;
-    meta.homepage = "https://github.com/catppuccin/nvim";
-  };
-
-  feline-nvim = vimUtils.buildVimPlugin {
-    pname = "feline-nvim";
-    version = inputs.feline-nvim.lastModifiedDate;
-    src = inputs.feline-nvim;
-    meta.homepage = "https://github.com/feline-nvim/feline.nvim";
-  };
-
-  nvim-web-devicons = vimUtils.buildVimPlugin {
-    pname = "nvim-web-devicons";
-    version = inputs.nvim-web-devicons.lastModifiedDate;
-    src = inputs.nvim-web-devicons;
-    meta.homepage = "https://github.com/kyazdani42/nvim-web-devicons";
-  };
-
-  hydra-nvim = vimUtils.buildVimPlugin {
-    pname = "hydra-nvim";
-    version = inputs.hydra-nvim.lastModifiedDate;
-    src = inputs.hydra-nvim;
-    meta.homepage = "https://github.com/anuvyklack/hydra.nvim";
-  };
-
-  gitsigns-nvim = vimUtils.buildVimPluginFrom2Nix {
-    pname = "gitsigns-nvim";
-    version = inputs.gitsigns-nvim.lastModifiedDate;
-    src = inputs.gitsigns-nvim;
-    meta.homepage = "https://github.com/lewis6991/gitsigns.nvim";
-  };
+}: let
+  build = name:
+    vimUtils.buildVimPluginFrom2Nix rec {
+      pname = name;
+      version = src.lastModifiedDate;
+      src = builtins.getAttr name inputs;
+    };
+in {
+  catppuccin-nvim = build "catppuccin-nvim";
+  feline-nvim = build "feline-nvim";
+  nvim-web-devicons = build "nvim-web-devicons";
+  hydra-nvim = build "hydra-nvim";
+  gitsigns-nvim = build "gitsigns-nvim";
 }
