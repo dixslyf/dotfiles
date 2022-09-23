4f7b6b07abe10137d8e5e69259b104f9eb5f15d6
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-09-23 16:44:21 +0800

    refactor(wlsunset): set duration and change night temperature
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/services/wlsunset/default.nix
+++ users/shiba/services/wlsunset/default.nix
@@ -5,6 +5,8 @@
     package = pkgs.pvtpkgs.wlsunset;
     sunrise = "07:00";
     sunset = "19:00";
+    duration = 60;
+    temperature.night = 3400;
     systemdTarget = "hyprland-session.target";
   };
 }
