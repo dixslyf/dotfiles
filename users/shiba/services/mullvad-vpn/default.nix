21a76e402ab5fb257e91bb576952a9d999aa5650
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-09 13:04:11 +0800

    feat(mullvad): make and use home-manager module
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/services/mullvad-vpn/default.nix
+++ users/shiba/services/mullvad-vpn/default.nix
@@ -1,12 +1,11 @@
 {pkgs, ...}: {
-  systemd.user.services.mullvad-vpn = {
-    Unit = {
-      Description = "Mullvad VPN GUI";
-      Requires = ["tray.target"];
-      After = ["graphical-session-pre.target" "tray.target"];
-      PartOf = ["graphical-session.target"];
+  imports = [./module.nix];
+  pvt.services.mullvad-vpn = {
+    enable = true;
+    settings = {
+      autoConnect = true;
+      startMinimized = true;
+      changelogDisplayedForVersion = pkgs.mullvad-vpn.version;
     };
-    Install = {WantedBy = ["graphical-session.target"];};
-    Service = {ExecStart = "${pkgs.mullvad-vpn}/bin/mullvad-vpn";};
   };
 }
