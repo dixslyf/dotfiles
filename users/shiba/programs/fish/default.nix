81be8e7243f288ea80d9da14fdd49c5c0c663aeb
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-13 23:34:09 +0800

    feat(fish): use catppuccin macchiato theme
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/fish/default.nix
+++ users/shiba/programs/fish/default.nix
@@ -1,8 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       fish_vi_key_bindings
+      fish_config theme choose "Catppuccin Macchiato"
     '';
+  };
+
+  xdg.configFile."fish/themes" = {
+    source = "${pkgs.pvtpkgs.catppuccin-fish}/share/fish/themes";
+    recursive = true;
   };
 }
