eafaf38fe0dc97c16d2e3e7efb7cea169abc031c
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-15 19:30:10 +0800

    fix(gpg-agent): update start up tty for fish
    Without this, trying to `git push` will fail saying that the gpg agent
    refused operation. See home-manager issue 3244.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/services/gpg-agent/default.nix
+++ users/shiba/services/gpg-agent/default.nix
@@ -1,7 +1,15 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.gpg-agent = {
     enable = true;
     enableFishIntegration = true;
     enableSshSupport = true;
   };
+
+  programs.fish.interactiveShellInit = lib.mkIf (config.services.gpg-agent.enableFishIntegration && config.services.gpg-agent.enableSshSupport) (lib.mkAfter ''
+    ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
+  '');
 }
