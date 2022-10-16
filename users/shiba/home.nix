5f6ff3182ec688ada85f1b9d0eb08a43feb9f842
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-16 14:22:14 +0800

    style(editorconfig): add .editorconfig in home
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/home.nix
+++ users/shiba/home.nix
@@ -65,4 +65,19 @@
       name = "Catppuccin-Macchiato-Mauve";
     };
   };
+
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
+        indent_style = "space";
+      };
+      "*.nix" = { indent_size = 2; };
+      "*.lua" = { indent_size = 3; };
+    };
+  };
 }
