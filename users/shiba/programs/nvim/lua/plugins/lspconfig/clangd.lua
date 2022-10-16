28de6d3ba02cb92b4db8d770945749bd93d17c8a
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-16 18:31:20 +0800

    fix(nvim/lsp): fix warning about different client offset encodings
    Fixed by setting the offset encoding to utf-16 for clangd. See null-ls
    issue 428.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/nvim/lua/plugins/lspconfig/clangd.lua
+++ users/shiba/programs/nvim/lua/plugins/lspconfig/clangd.lua
@@ -1,7 +1,11 @@
 local function setup(on_attach)
+   local capabilities = vim.lsp.protocol.make_client_capabilities()
+   capabilities.offsetEncoding = "utf-16"
+
    local lspconfig = require("lspconfig")
    lspconfig.clangd.setup({
-      on_attach = on_attach
+      on_attach = on_attach,
+      capabilities = capabilities
    })
 end
 
