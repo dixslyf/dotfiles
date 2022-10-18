667edff6034e8da749e7b5acd73179a5d182dbac
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-18 22:33:04 +0800

    feat(nvim): add toggle binding for tint
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/nvim/lua/plugins/tint.lua
+++ users/shiba/programs/nvim/lua/plugins/tint.lua
@@ -1,4 +1,8 @@
-require("tint").setup({
+local tint = require("tint")
+
+tint.setup({
    tint = -50,
    saturation = 0.5,
 })
+
+vim.keymap.set("n", "<leader>ti", tint.toggle, { desc = "Toggle tint" })
