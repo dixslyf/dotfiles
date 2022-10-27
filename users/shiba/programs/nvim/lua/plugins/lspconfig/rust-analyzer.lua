4630d786290a0ee6a6b7e48062a5c9515ba2eebd
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-27 12:48:26 +0800

    feat(nvim/lsp): use clippy for rust-analyzer checkOnSave
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/nvim/lua/plugins/lspconfig/rust-analyzer.lua
+++ users/shiba/programs/nvim/lua/plugins/lspconfig/rust-analyzer.lua
@@ -2,6 +2,21 @@ local function setup(on_attach)
    local lspconfig = require("lspconfig")
    lspconfig.rust_analyzer.setup({
       on_attach = on_attach,
+      settings = {
+         ["rust-analyzer"] = {
+            checkOnSave = {
+               allFeatures = true,
+               overrideCommand = {
+                  "cargo",
+                  "clippy",
+                  "--workspace",
+                  "--message-format=json",
+                  "--all-targets",
+                  "--all-features",
+               },
+            },
+         },
+      },
    })
 end
 
