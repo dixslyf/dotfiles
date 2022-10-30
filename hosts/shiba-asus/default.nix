d21665e57d694aa79ec7389ab17add6554823d36
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-30 16:05:42 +0800

    feat(cachix): use my cachix
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- hosts/shiba-asus/default.nix
+++ hosts/shiba-asus/default.nix
@@ -62,11 +62,13 @@
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
       substituters = [
+        "https://playernamehere-nixos.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "playernamehere-nixos.cachix.org-1:oUHiXCIHbB+VbjDIlckfjwaYkJEvpkW3250+FhF5Vi4="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
