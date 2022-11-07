33da52e26d4c4b76ccaee1bc76c3dbd3ef0f2ee4
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-11-07 16:03:48 +0800

    refactor(ssh): disable sshd
    Originally enabled to generate the ssh keys for the machine to use with
    sops-nix. That could have been done using ssh-keygen and doesn't
    actually require enabling openssh's daemon. Since I don't ssh into this
    machine, there's no point having the daemon running.
    
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- hosts/shiba-asus/default.nix
+++ hosts/shiba-asus/default.nix
@@ -171,15 +171,6 @@
       pulse.enable = true;
       lowLatency.enable = true;
     };
-    openssh = {
-      enable = true;
-      hostKeys = [
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
-    };
     resolved.enable = true;
     mullvad-vpn.enable = true;
     psd.enable = true;
