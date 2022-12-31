304b83b916d30000a44824e67214ebb0e6cb31be
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-12-31 17:23:00 +0800

    refactor(nvim,tint): ignore some highlight groups
    Signed-off-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- users/shiba/programs/nvim/lua/plugins/tint.lua
+++ users/shiba/programs/nvim/lua/plugins/tint.lua
@@ -3,6 +3,7 @@ local transforms = require("tint.transforms")
 local cp = require("catppuccin.palettes").get_palette()
 
 tint.setup({
+   highlight_ignore_patterns = { "WinSeparator", "EndOfBuffer" },
    transforms = {
       transforms.tint_with_threshold(-50, cp.base, 50),
       transforms.saturate(0.5),
