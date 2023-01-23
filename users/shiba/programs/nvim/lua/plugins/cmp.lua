4907a08c0d33feb07dade48245e99a2b3e61f5a0
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-01-23 19:00:07 +0800

    feat(nvim, cmp): use super-tab mapping

--- users/shiba/programs/nvim/lua/plugins/cmp.lua
+++ users/shiba/programs/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
    formatting = {
@@ -8,7 +9,6 @@ cmp.setup({
    },
    snippet = {
       expand = function(args)
-         local luasnip = require("luasnip")
          luasnip.lsp_expand(args.body)
       end,
    },
@@ -17,6 +17,31 @@ cmp.setup({
       -- documentation = cmp.config.window.bordered(),
    },
    mapping = cmp.mapping.preset.insert({
+      ["<Tab>"] = cmp.mapping(function(fallback)
+         local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+         end
+
+         if cmp.visible() then
+            cmp.select_next_item()
+         elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+         elseif has_words_before() then
+            cmp.complete()
+         else
+            fallback()
+         end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+         if cmp.visible() then
+            cmp.select_prev_item()
+         elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+         else
+            fallback()
+         end
+      end, { "i", "s" }),
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
