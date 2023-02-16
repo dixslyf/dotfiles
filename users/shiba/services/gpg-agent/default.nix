d70df559a992da73e1a3a171754fd8d631198a45
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-16 12:49:30 +0800

    revert: "fix(gpg-agent): update start up tty for fish"
    This reverts commit eafaf38fe0dc97c16d2e3e7efb7cea169abc031c.
    No longer needed, fixed in home-manager 3576.

--- users/shiba/services/gpg-agent/default.nix
+++ users/shiba/services/gpg-agent/default.nix
@@ -1,15 +1,7 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{...}: {
   services.gpg-agent = {
     enable = true;
     enableFishIntegration = true;
     enableSshSupport = true;
   };
-
-  programs.fish.interactiveShellInit = lib.mkIf (config.services.gpg-agent.enableFishIntegration && config.services.gpg-agent.enableSshSupport) (lib.mkAfter ''
-    ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
-  '');
 }
