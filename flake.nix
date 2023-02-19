4a20bb5075694fddb844e7702df780fa2b53120d
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-19 15:49:04 +0800

    feat(flake): add cachix-deploy-spec
    Inspired by wagdav's homelab.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    cachix-deploy = {
+      url = "github:cachix/cachix-deploy-flake";
+      inputs.home-manager.follows = "home-manager";
+    };
     impermanence.url = "github:nix-community/impermanence";
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -36,29 +40,43 @@
     pers-pkgs.url = "github:PlayerNameHere/nix-pers-pkgs";
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    pers-pkgs,
-    ...
-  }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-    nixosConfigurations = {
-      shiba-asus = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          {
-            nixpkgs.overlays = [
-              inputs.rust-overlay.overlays.default
-              inputs.neovim-nightly-overlay.overlay
-              pers-pkgs.overlay
-            ];
-            nix.registry.nixpkgs.flake = nixpkgs;
-          }
-          ./hosts/shiba-asus
-        ];
-        specialArgs = {inherit inputs;};
+  outputs =
+    inputs @ { self
+    , nixpkgs
+    , pers-pkgs
+    , cachix-deploy
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      cachix-deploy-lib = cachix-deploy.lib pkgs;
+    in
+    {
+      formatter.${system} = pkgs.alejandra;
+
+      nixosConfigurations = {
+        shiba-asus = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              nixpkgs.overlays = [
+                inputs.rust-overlay.overlays.default
+                inputs.neovim-nightly-overlay.overlay
+                pers-pkgs.overlay
+              ];
+              nix.registry.nixpkgs.flake = nixpkgs;
+            }
+            ./hosts/shiba-asus
+          ];
+          specialArgs = { inherit inputs; };
+        };
+      };
+
+      packages.${system}.cachix-deploy-spec = cachix-deploy-lib.spec {
+        agents = {
+          shiba-asus = self.nixosConfigurations.shiba-asus.config.system.build.toplevel;
+        };
       };
     };
-  };
 }
