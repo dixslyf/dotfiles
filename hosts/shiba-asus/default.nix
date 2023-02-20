b66e310a13633668e228a3acc75e0e3a62e48742
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-20 23:01:37 +0800

    feat(devenv): add devenv cachix substituter

--- hosts/shiba-asus/default.nix
+++ hosts/shiba-asus/default.nix
@@ -67,12 +67,14 @@
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
+        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "playernamehere-nixos.cachix.org-1:oUHiXCIHbB+VbjDIlckfjwaYkJEvpkW3250+FhF5Vi4="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
     extraOptions = ''
