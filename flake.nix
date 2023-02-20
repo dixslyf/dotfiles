c9d2daff3dd4bf4eda2df54289e564bf3bd113c0
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-20 22:49:04 +0800

    feat(devenv): add stylua pre-commit hook

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
             packages = with pkgs; [ sops ];
             pre-commit.hooks = {
               nixpkgs-fmt.enable = true;
+              stylua.enable = true;
             };
           }
         ];
