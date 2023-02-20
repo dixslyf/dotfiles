014342ef5a5b03534604a6cf368c032a6fc09bc8
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-20 22:05:07 +0800

    refactor(nvim, lsp): disable formatting through settings for lua-ls instead of through capabilities
    Seems like changing server capabilities is not really the best way.

--- users/shiba/programs/nvim/lua/plugins/lspconfig/lua-ls.lua
+++ users/shiba/programs/nvim/lua/plugins/lspconfig/lua-ls.lua
@@ -1,12 +1,16 @@
 local function setup(on_attach, capabilities)
    require("neodev").setup()
 
-   -- Disable formatting capabilities to prevent conflict with null-ls stylua
-   capabilities.documentFormattingProvider = false
-
    require("lspconfig").lua_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
+      settings = {
+         Lua = {
+            format = {
+               enable = false,
+            },
+         },
+      },
    })
 end
 
