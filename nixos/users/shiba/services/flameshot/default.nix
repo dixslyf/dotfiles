8c7fff1f69ed5fb146613a9c3e4569f708c0247a
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-02-24 21:53:14 +0800

    refactor(flameshot): remove `tray.target` requirement
    Since I disabled the tray for flameshot, I don't need the unit to depend
    on `tray.target`.

--- nixos/users/shiba/services/flameshot/default.nix
+++ nixos/users/shiba/services/flameshot/default.nix
@@ -10,6 +10,10 @@
   };
 
   systemd.user.services.flameshot = {
+    # the original configuration lists `tray.target` as a requirement,
+    # which the below removes
+    Unit.Requires = lib.mkForce [ ];
+    Unit.After = lib.mkForce [ "bspwm-session.target" ];
     # run only in bspwm
     Unit.PartOf = lib.mkForce [ "bspwm-session.target" ];
     Install.WantedBy = lib.mkForce [ "bspwm-session.target" ];
