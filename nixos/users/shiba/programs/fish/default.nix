7ba53ed7b724aa9e879ac2fd302b4a486f4b9317
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-03-09 12:14:38 +0800

    refactor(fish): set login shell init

--- nixos/users/shiba/programs/fish/default.nix
+++ nixos/users/shiba/programs/fish/default.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }: {
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      fish_vi_key_bindings
-      fish_config theme choose "Catppuccin Macchiato"
-    '';
-  };
+  programs.fish =
+    let
+      shellInit = ''
+        fish_vi_key_bindings
+        fish_config theme choose "Catppuccin Macchiato"
+      '';
+    in
+    {
+      enable = true;
+      loginShellInit = shellInit;
+      interactiveShellInit = shellInit;
+    };
 
   xdg.configFile."fish/themes" = {
     source = "${pkgs.pers-pkgs.catppuccin-fish}/share/fish/themes";
