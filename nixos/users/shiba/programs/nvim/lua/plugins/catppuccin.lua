a21d5d0e688107cb27d1c26b1d006a607d0fe2bf
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-03-10 18:13:46 +0800

    feat(nvim, catppuccin): enable non-special integrations

--- nixos/users/shiba/programs/nvim/lua/plugins/catppuccin.lua
+++ nixos/users/shiba/programs/nvim/lua/plugins/catppuccin.lua
@@ -3,10 +3,17 @@ vim.g.catppuccin_flavour = "macchiato"
 require("catppuccin").setup({
    compile_path = vim.fn.stdpath("cache") .. "/catppuccin-nvim",
    integrations = {
+      gitsigns = true,
       indent_blankline = {
          enabled = true,
          colored_indent_levels = false,
       },
+      leap = true,
+      markdown = true,
+      cmp = true,
+      treesitter = true,
+      telescope = true,
+      which_key = true,
    },
 })
 
