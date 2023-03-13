d005fb7568f254103280b503393b0702bbc95a31
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-03-13 11:44:46 +0800

    feat(nvim, telescope): add bindings to show/grep all files

--- nixos/users/shiba/programs/nvim/lua/plugins/telescope.lua
+++ nixos/users/shiba/programs/nvim/lua/plugins/telescope.lua
@@ -15,7 +15,15 @@ telescope.setup({
 telescope.load_extension("fzf")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Files" })
+vim.keymap.set("n", "<leader>fF", function()
+   builtin.find_files({ hidden = true, no_ignore = true })
+end, { desc = "Files (all)" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep" })
+vim.keymap.set("n", "<leader>fG", function()
+   builtin.live_grep({
+      additional_args = { "--no-ignore", "--hidden" },
+   })
+end, { desc = "Grep (all)" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
 vim.keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "Man pages" })
