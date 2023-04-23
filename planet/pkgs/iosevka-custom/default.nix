49fb4bd3490583101000d2506b01f24eeb287eee
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-04-23 12:21:29 +0800

    revert: "refactor(planet, pkgs): move iosevka private build plan to separate file"
    This reverts commit 89476323550ed715ca3b1691c430c50ca9caee7f.
    
    This seems to actually be an instance of IFD, which `nix flake show`
    does not allow by default. I think it's better to avoid IFD than to
    enable it.

--- planet/pkgs/iosevka-custom/default.nix
+++ planet/pkgs/iosevka-custom/default.nix
@@ -1,5 +1,4 @@
-{ pkgs
-, iosevka
+{ iosevka
 , spacing ? "normal"
 , ...
 }:
@@ -12,12 +11,48 @@ let
     else throw "Unsupported spacing";
   set = builtins.head names;
   set-family = builtins.elemAt names 1;
-  plan = pkgs.substituteAll {
-    src = ./private-build-plans.toml;
-    inherit set set-family spacing;
-  };
 in
 iosevka.override {
   inherit set;
-  privateBuildPlan = builtins.readFile plan;
+  privateBuildPlan = ''
+    [buildPlans.iosevka-${set}]
+    family = "Iosevka ${set-family}"
+    spacing = "${spacing}"
+    serifs = "sans"
+    no-cv-ss = true
+    export-glyph-names = true
+
+      [buildPlans.iosevka-${set}.variants]
+      inherits = "ss05"
+
+        [buildPlans.iosevka-${set}.variants.design]
+        capital-d = "standard-serifless"
+        capital-k = "straight-serifless"
+        capital-q = "detached-bend-tailed"
+        f = "tailed"
+        q = "earless-rounded-diagonal-tailed"
+        long-s = "flat-hook-tailed"
+        capital-gamma = "serifless"
+        zero = "dotted"
+        number-sign = "slanted-open"
+        ampersand = "upper-open"
+        at = "short"
+        dollar = "interrupted-cap"
+        percent = "dots"
+        bar = "natural-slope"
+        lig-neq = "more-slanted"
+        lig-equal-chain = "without-notch"
+        lig-hyphen-chain = "with-notch"
+        lig-double-arrow-bar = "without-notch"
+        lig-single-arrow-bar = "without-notch"
+
+      [buildPlans.iosevka-${set}.ligations]
+      inherits = "dlig"
+      enables = ["connected-number-sign"]
+      disables = [
+        "connected-tilde-as-wave",
+        "connected-underscore",
+        "connected-hyphen"
+      ]
+  '';
 }
