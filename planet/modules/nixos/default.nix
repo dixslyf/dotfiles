611a4346cb67b3f5bcc115e1f9b2230eae9ae990
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-12 20:23:42 +0800

    refactor(planet, nixos): flatten

--- planet/modules/nixos/default.nix
+++ planet/modules/nixos/default.nix
@@ -1,10 +1,18 @@
 _: {
   imports = [
-    ./persistence
+    ./btrfs
+    ./earlyoom
+    ./hyprland
+    ./mullvad-vpn
+    ./neovim
     ./network-manager
     ./nvidia
-    ./programs
-    ./services
+    ./persistence
+    ./pipewire
+    ./steam
+    ./tlp
+    ./udisks2
+    ./upower
     ./xdg
   ];
 }
--- planet/modules/nixos/programs/default.nix
+++
@@ -1,7 +0,0 @@
-_: {
-  imports = [
-    ./hyprland
-    ./neovim
-    ./steam
-  ];
-}
