ca0dc6f90e0e095d72279c73a7478ebee89b5ce0
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-14 18:23:46 +0800

    refactor(hm): remove redundant module import

--- home-manager/flake-module.nix
+++ home-manager/flake-module.nix
@@ -25,15 +25,11 @@ let
   };
 in
 {
-  imports = [
-    # Make `homeUsers` available to other flake modules
-    # In particular, make it available to the NixOS configuration
-    {
-      _module.args = {
-        inherit homeUsers;
-      };
-    }
-  ];
+  # Make `homeUsers` available to other flake modules
+  # In particular, make it available to the NixOS configuration
+  _module.args = {
+    inherit homeUsers;
+  };
 
   flake = {
     homeConfigurations = withSystem "x86_64-linux"
