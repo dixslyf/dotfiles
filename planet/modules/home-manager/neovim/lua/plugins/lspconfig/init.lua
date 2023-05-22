2b5417eed6c365f5465f1412027b966ce87f8e80
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-22 15:25:45 +0800

    refactor(planet, hm, neovim): return table from `plugins.lspconfig`

--- planet/modules/home-manager/neovim/lua/plugins/lspconfig/init.lua
+++ planet/modules/home-manager/neovim/lua/plugins/lspconfig/init.lua
@@ -1,18 +1,20 @@
--- Mappings.
+local M = {}
+
 local wk = require("which-key")
 local tls_builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { silent = true, desc = "Show diagnostics" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true, desc = "Next diagnostic" })
-vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { silent = true, desc = "Next diagnostic" })
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true, desc = "Previous diagnostic" })
-vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { silent = true, desc = "Previous diagnostic" })
-vim.keymap.set("n", "<leader>dd", tls_builtin.diagnostics, { silent = true, desc = "List diagnostics" })
-
-wk.register({ ["<leader>d"] = "Diagnostics" })
-
--- Set up servers
 local common = require("plugins.lspconfig.common")
+
+local function setup_mappings()
+   wk.register({ ["<leader>d"] = "Diagnostics" })
+
+   vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { silent = true, desc = "Show diagnostics" })
+   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true, desc = "Next diagnostic" })
+   vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { silent = true, desc = "Next diagnostic" })
+   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true, desc = "Previous diagnostic" })
+   vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { silent = true, desc = "Previous diagnostic" })
+   vim.keymap.set("n", "<leader>dd", tls_builtin.diagnostics, { silent = true, desc = "List diagnostics" })
+end
+
 local servers = {
    "null-ls",
    "nil-ls",
@@ -24,7 +26,16 @@ local servers = {
    "hls",
 }
 
-for _, server in ipairs(servers) do
-   local capabilities = common.capabilities()
-   require("plugins.lspconfig." .. server).setup(common.on_attach, capabilities)
+local function setup_servers()
+   for _, server in ipairs(servers) do
+      local capabilities = common.capabilities()
+      require("plugins.lspconfig." .. server).setup(common.on_attach, capabilities)
+   end
 end
+
+function M.setup()
+   setup_mappings()
+   setup_servers()
+end
+
+return M
