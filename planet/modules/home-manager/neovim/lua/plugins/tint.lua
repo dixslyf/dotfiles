5f6b8d64c1dfec819ed7ef0376bdb1dcf3f2c44e
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-24 17:17:01 +0800

    refactor(planet, hm, neovim): use table for tint module

--- planet/modules/home-manager/neovim/lua/plugins/tint.lua
+++ planet/modules/home-manager/neovim/lua/plugins/tint.lua
@@ -1,13 +1,20 @@
+local M = {}
+
 local tint = require("tint")
 local transforms = require("tint.transforms")
-local cp = require("catppuccin.palettes").get_palette()
+local cp = require("catppuccin.palettes")
 
-tint.setup({
-   highlight_ignore_patterns = { "WinSeparator", "EndOfBuffer" },
-   transforms = {
-      transforms.tint_with_threshold(-50, cp.base, 50),
-      transforms.saturate(0.5),
-   },
-})
+function M.setup()
+   local cp_palette = cp.get_palette()
+   tint.setup({
+      highlight_ignore_patterns = { "WinSeparator", "EndOfBuffer" },
+      transforms = {
+         transforms.tint_with_threshold(-50, cp_palette.base, 50),
+         transforms.saturate(0.5),
+      },
+   })
 
-vim.keymap.set("n", "<leader>ni", tint.toggle, { desc = "Toggle tint" })
+   vim.keymap.set("n", "<leader>ni", tint.toggle, { desc = "Toggle tint" })
+end
+
+return M
