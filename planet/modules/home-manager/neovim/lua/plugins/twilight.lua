f5399c40963f7939e69a42c73642274d847b706c
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-24 17:17:41 +0800

    refactor(planet, hm, neovim): use table for twilight module

--- planet/modules/home-manager/neovim/lua/plugins/twilight.lua
+++ planet/modules/home-manager/neovim/lua/plugins/twilight.lua
@@ -1,6 +1,12 @@
+local M = {}
+
 local twilight = require("twilight")
 local wk = require("which-key")
-wk.register({ ["<leader>n"] = "Dim" })
 
-twilight.setup()
-vim.keymap.set("n", "<leader>nt", twilight.toggle, { desc = "Toggle twilight" })
+function M.setup()
+   wk.register({ ["<leader>n"] = "Dim" })
+   twilight.setup()
+   vim.keymap.set("n", "<leader>nt", twilight.toggle, { desc = "Toggle twilight" })
+end
+
+return M
