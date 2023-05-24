a1f565d68fa95c15771b32cc5201977c8d99b7b0
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-24 17:16:21 +0800

    refactor(planet, hm, neovim): use table for markdown-preview module

--- planet/modules/home-manager/neovim/lua/plugins/markdown-preview.lua
+++ planet/modules/home-manager/neovim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,12 @@
+local M = {}
+
 local wk = require("which-key")
 
-vim.keymap.set("n", "<leader>mm", "<Plug>MarkdownPreview", { remap = true, desc = "Preview" })
-vim.keymap.set("n", "<leader>ms", "<Plug>MarkdownPreviewStop", { remap = true, desc = "Stop previewing" })
-vim.keymap.set("n", "<leader>mt", "<Plug>MarkdownPreviewToggle", { remap = true, desc = "Toggle preview" })
-wk.register({ ["<leader>m"] = "Markdown" })
+function M.setup()
+   wk.register({ ["<leader>m"] = "Markdown" })
+   vim.keymap.set("n", "<leader>mm", "<Plug>MarkdownPreview", { remap = true, desc = "Preview" })
+   vim.keymap.set("n", "<leader>ms", "<Plug>MarkdownPreviewStop", { remap = true, desc = "Stop previewing" })
+   vim.keymap.set("n", "<leader>mt", "<Plug>MarkdownPreviewToggle", { remap = true, desc = "Toggle preview" })
+end
+
+return M
