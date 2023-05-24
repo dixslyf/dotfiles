6f1798f9ef8258d61155161dae323504b2a32807
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-24 17:17:12 +0800

    refactor(planet, hm, neovim): use table for todo-comments module

--- planet/modules/home-manager/neovim/lua/plugins/todo-comments.lua
+++ planet/modules/home-manager/neovim/lua/plugins/todo-comments.lua
@@ -1,13 +1,20 @@
+local M = {}
+
 local todo = require("todo-comments")
 local telescope = require("telescope")
-todo.setup({
-   highlight = {
-      -- Sometimes there are todos in strings, especially in Nix
-      -- where strings may be bash and the todos are actually bash comments
-      comments_only = false,
-   },
-})
 
-vim.keymap.set("n", "<leader>ft", telescope.extensions["todo-comments"].todo, { desc = "Todo comments" })
-vim.keymap.set("n", "]t", todo.jump_next, { desc = "Next todo comment" })
-vim.keymap.set("n", "[t", todo.jump_prev, { desc = "Previous todo comment" })
+function M.setup()
+   todo.setup({
+      highlight = {
+         -- Sometimes there are todos in strings, especially in Nix
+         -- where strings may be bash and the todos are actually bash comments
+         comments_only = false,
+      },
+   })
+
+   vim.keymap.set("n", "<leader>ft", telescope.extensions["todo-comments"].todo, { desc = "Todo comments" })
+   vim.keymap.set("n", "]t", todo.jump_next, { desc = "Next todo comment" })
+   vim.keymap.set("n", "[t", todo.jump_prev, { desc = "Previous todo comment" })
+end
+
+return M
