e93d41e38834cf868c1b59090a2fa64d87ea449d
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-24 17:13:57 +0800

    refactor(planet, hm, neovim): use table for dap module

--- planet/modules/home-manager/neovim/lua/plugins/dap.lua
+++ planet/modules/home-manager/neovim/lua/plugins/dap.lua
@@ -1,54 +1,69 @@
+local M = {}
+
 local dap = require("dap")
 local dapui = require("dapui")
 local wk = require("which-key")
 
 -- Catppuccin
-vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
-vim.fn.sign_define(
-   "DapBreakpointCondition",
-   { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" }
-)
-vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+local function catppuccin_integration()
+   vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+   vim.fn.sign_define(
+      "DapBreakpointCondition",
+      { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" }
+   )
+   vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+end
 
-vim.keymap.set("n", "<leader>Db", dap.toggle_breakpoint, { silent = true, desc = "Toggle breakpoint" })
-vim.keymap.set("n", "<leader>Dc", dap.continue, { silent = true, desc = "Continue" })
-vim.keymap.set("n", "<leader>DC", dap.reverse_continue, { silent = true, desc = "Reverse continue" })
-vim.keymap.set("n", "<leader>Ds", dap.step_over, { silent = true, desc = "Step over" })
-vim.keymap.set("n", "<leader>DS", dap.step_back, { silent = true, desc = "Step back" })
-vim.keymap.set("n", "<leader>Df", dap.step_into, { silent = true, desc = "Step into" })
-vim.keymap.set("n", "<leader>DF", dap.step_out, { silent = true, desc = "Step out" })
-vim.keymap.set("n", "<leader>Dt", dap.terminate, { silent = true, desc = "Terminate" })
-vim.keymap.set("n", "<leader>DD", dapui.toggle, { silent = true, desc = "Toggle UI" })
+local function setup_mappings()
+   wk.register({ ["<leader>D"] = "Debug" })
+   vim.keymap.set("n", "<leader>Db", dap.toggle_breakpoint, { silent = true, desc = "Toggle breakpoint" })
+   vim.keymap.set("n", "<leader>Dc", dap.continue, { silent = true, desc = "Continue" })
+   vim.keymap.set("n", "<leader>DC", dap.reverse_continue, { silent = true, desc = "Reverse continue" })
+   vim.keymap.set("n", "<leader>Ds", dap.step_over, { silent = true, desc = "Step over" })
+   vim.keymap.set("n", "<leader>DS", dap.step_back, { silent = true, desc = "Step back" })
+   vim.keymap.set("n", "<leader>Df", dap.step_into, { silent = true, desc = "Step into" })
+   vim.keymap.set("n", "<leader>DF", dap.step_out, { silent = true, desc = "Step out" })
+   vim.keymap.set("n", "<leader>Dt", dap.terminate, { silent = true, desc = "Terminate" })
+   vim.keymap.set("n", "<leader>DD", dapui.toggle, { silent = true, desc = "Toggle UI" })
+end
 
-wk.register({ ["<leader>D"] = "Debug" })
+function M.setup()
+   dap.adapters.cppdbg = {
+      id = "cppdbg",
+      type = "executable",
+      command = Globals.cppdbg_command,
+   }
 
-dap.adapters.cppdbg = {
-   id = "cppdbg",
-   type = "executable",
-   command = Globals.cppdbg_command,
-}
-
-dap.configurations.cpp = {
-   {
-      name = "Launch file",
-      type = "cppdbg",
-      request = "launch",
-      program = function()
-         return vim.fn.input({ prompt = "Path to executable: ", default = vim.fn.getcwd() .. "/", completion = "file" })
-      end,
-      cwd = "${workspaceFolder}",
-      stopAtEntry = true,
-      setupCommands = {
-         {
-            text = "-enable-pretty-printing",
-            description = "enable pretty printing",
-            ignoreFailures = false,
+   dap.configurations.cpp = {
+      {
+         name = "Launch file",
+         type = "cppdbg",
+         request = "launch",
+         program = function()
+            return vim.fn.input({
+               prompt = "Path to executable: ",
+               default = vim.fn.getcwd() .. "/",
+               completion = "file",
+            })
+         end,
+         cwd = "${workspaceFolder}",
+         stopAtEntry = true,
+         setupCommands = {
+            {
+               text = "-enable-pretty-printing",
+               description = "enable pretty printing",
+               ignoreFailures = false,
+            },
          },
       },
-   },
-}
+   }
 
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+   dap.configurations.c = dap.configurations.cpp
+   dap.configurations.rust = dap.configurations.cpp
 
-dapui.setup()
+   dapui.setup()
+   catppuccin_integration()
+   setup_mappings()
+end
+
+return M
