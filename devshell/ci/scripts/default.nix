76760c80ef32913847f1570367cb8f916a0a381b
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-25 15:12:54 +0800

    feat(devshell, ci): add script to configure git user

--- devshell/ci/scripts/default.nix
+++ devshell/ci/scripts/default.nix
@@ -28,4 +28,17 @@
       ];
     }
     (builtins.readFile ./generate-npins-matrix.sh);
+
+  configure-git-user = pkgs.resholve.writeScriptBin "configure-git-user.sh"
+    {
+      interpreter = "${pkgs.bash}/bin/bash";
+      inputs = with pkgs; [
+        coreutils
+        git
+      ];
+      execer = [
+        "cannot:${pkgs.git}/bin/git"
+      ];
+    }
+    (builtins.readFile ./configure-git-user.sh);
 }
