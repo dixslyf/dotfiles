71e2c9b3a633deca94e41159136941a7299a1822
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-05-25 14:01:02 +0800

    feat(devshell, ci): add scripts to devshell packages

--- devshell/ci/default.nix
+++ devshell/ci/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  scripts = import ./scripts { inherit pkgs; };
+in
 {
-  packages = with pkgs; [ npins ];
+  packages = with pkgs; [ npins ] ++ (pkgs.lib.attrValues scripts);
 }
