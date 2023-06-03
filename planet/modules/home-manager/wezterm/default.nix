b9c3896abc2decc6b60d37809cbd9687d4b4ae1e
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-06-03 13:28:48 +0800

    feat(planet, hm, wezterm): add option to set as default terminal

--- planet/modules/home-manager/wezterm/default.nix
+++ planet/modules/home-manager/wezterm/default.nix
@@ -11,31 +11,47 @@
     {
       planet.wezterm = {
         enable = mkEnableOption "planet wezterm";
+        defaultTerminal = mkEnableOption "wezterm as the default terminal";
       };
     };
 
   config =
     let
       cfg = config.planet.wezterm;
-      inherit (lib) mkIf;
+      inherit (lib)
+        mkIf
+        mkMerge;
     in
-    mkIf cfg.enable {
-      home.packages = with pkgs; [
-        material-design-icons
-        localFlake'.packages.iosevka-term-custom
-        (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-      ];
+    mkIf cfg.enable (mkMerge [
+      {
+        home.packages = with pkgs; [
+          material-design-icons
+          localFlake'.packages.iosevka-term-custom
+          (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+        ];
 
-      programs.wezterm = {
-        enable = true;
-      };
+        programs.wezterm = {
+          enable = true;
+        };
 
-      xdg.configFile."wezterm/wezterm.lua".source = pkgs.substituteAll {
-        src = pkgs.runCommand "wezterm.lua" { } ''
-          ${pkgs.luaPackages.fennel}/bin/fennel --compile ${./wezterm.fnl} > "$out"
-        '';
-        default_shell = "${pkgs.fish}/bin/fish";
-      };
-    };
+        xdg.configFile."wezterm/wezterm.lua".source = pkgs.substituteAll {
+          src = pkgs.runCommand "wezterm.lua" { } ''
+            ${pkgs.luaPackages.fennel}/bin/fennel --compile ${./wezterm.fnl} > "$out"
+          '';
+          default_shell = "${pkgs.fish}/bin/fish";
+        };
+      }
+
+      (mkIf cfg.defaultTerminal {
+        planet.default-terminal = {
+          startCommand = ''
+            ${config.programs.wezterm.package}/bin/wezterm start -- "$@"
+          '';
+          startInDirectoryCommand = ''
+            ${config.programs.wezterm.package}/bin/wezterm start --cwd "$@"
+          '';
+        };
+      })
+    ]);
 }
 
