b5ca75a3c50155ad703ff7750785f45184ab8556
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-06-08 12:27:41 +0800

    feat(planet, hm, fzf): set colours to catppuccin macchiato

--- planet/modules/home-manager/fzf/default.nix
+++ planet/modules/home-manager/fzf/default.nix
@@ -24,6 +24,21 @@
         defaultCommand = "${pkgs.fd}/bin/fd --type f";
         fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
         changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+        colors = {
+          # Macchiato from https://github.com/catppuccin/fzf
+          fg = "#cad3f5";
+          "fg+" = "#cad3f5";
+          bg = "#24273a";
+          "bg+" = "#363a4f";
+          hl = "#ed8796";
+          "hl+" = "#ed8796";
+          spinner = "#f4dbd6";
+          header = "#ed8796";
+          info = "#c6a0f6";
+          pointer = "#f4dbd6";
+          marker = "#f4dbd6";
+          prompt = "#c6a0f6";
+        };
         # Shell integrations are set to true by default, so no need to enable them
       };
     };
