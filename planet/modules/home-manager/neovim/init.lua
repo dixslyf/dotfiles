0ba2faa1f165413a88bf0cb965230ad994e588b1
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-09-15 18:01:08 +0800

    refactor(planet/hm/neovim): move `extraLuaConfig` to a separate file
    Allows the language server for Lua to take effect on the contents of `extraLuaConfig`.

---
+++ planet/modules/home-manager/neovim/init.lua
@@ -0,0 +1,9 @@
+Globals = {
+   cppdbg_command = "@cppdbg_command@",
+   jdt_ls = "@jdt_ls@",
+   java_debug_server_dir = "@java_debug_server_dir@",
+   java_test_server_dir = "@java_test_server_dir@",
+}
+
+require("options").setup()
+require("plugins").setup()
