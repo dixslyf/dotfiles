f3d0358d971896a4b58b04d1aa23c343c94736a8
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-09-16 20:53:10 +0800

    feat(planet/nixos/flatpak): init

---
+++ planet/modules/nixos/flatpak/default.nix
@@ -0,0 +1,29 @@
+{ config
+, lib
+, ...
+}: {
+  options =
+    let
+      inherit (lib) mkEnableOption;
+    in
+    {
+      planet.flatpak = {
+        enable = mkEnableOption "planet flatpak";
+      };
+    };
+
+  config =
+    let
+      cfg = config.planet.flatpak;
+      inherit (lib) mkIf;
+    in
+    mkIf cfg.enable {
+      services.flatpak = {
+        enable = true;
+      };
+
+      planet.persistence = {
+        directories = [ "/var/lib/flatpak" ];
+      };
+    };
+}
