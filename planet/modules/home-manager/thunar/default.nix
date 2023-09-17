38c75a31d7b4b8b077da19c0b5f00193a05f52cb
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-09-17 13:40:06 +0800

    fix(planet/hm/thunar): remove D-Bus-activated user service
    Fixes #16.

--- planet/modules/home-manager/thunar/default.nix
+++ planet/modules/home-manager/thunar/default.nix
@@ -31,11 +31,28 @@
         src = ./uca.xml;
         startInDirectoryScript = pkgs.writeShellScript "exec-terminal-in-directory" config.planet.default-terminal.startInDirectoryCommand;
       };
+      # Remove the `thunar` user service that is activated by D-Bus.
+      #
+      # Unfortunately, it's not as straightforward. `pkgs.xfce.thunar` uses
+      # `symlinkJoin` when there are plugins. Under the hood, `symlinkJoin`
+      # uses `runCommand`, which uses the `buildCommand` argument to
+      # `stdenv.mkDerivation`. Annoyingly, `buildCommand` disables the
+      # usual phases, so we cannot simply override `postInstall` or any other
+      # phase attribute. Instead, we have to override `buildCommand` itself.
+      # More context is available in the comment below:
+      # https://github.com/NixOS/nixpkgs/issues/66826#issuecomment-1675224338
+      thunar = (pkgs.xfce.thunar.override {
+        thunarPlugins = [ pkgs.xfce.thunar-archive-plugin ];
+      }).overrideAttrs (previousAttrs: {
+        buildCommand = previousAttrs.buildCommand + ''
+          rm "$out/share/systemd/user/thunar.service"
+          rmdir "$out/share/systemd/user"
+          rmdir "$out/share/systemd"
+        '';
+      });
     in
     mkIf cfg.enable {
-      home.packages = with pkgs; [
-        (xfce.thunar.override { thunarPlugins = [ xfce.thunar-archive-plugin ]; })
-      ];
+      home.packages = [ thunar ];
       xdg.configFile."Thunar/uca.xml".source = uca;
       xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
         lib.genAttrs cfg.defaultApplication.mimeTypes (_: "thunar.desktop")
