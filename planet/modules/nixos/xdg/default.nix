c4c8ac076ba7f0023ac34d3407f3ddab927bf48c
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-12-01 18:09:57 +0800

    fix(planet/nixos/xdg): add configuration for portals

--- planet/modules/nixos/xdg/default.nix
+++ planet/modules/nixos/xdg/default.nix
@@ -24,6 +24,15 @@
           enable = true;
           # xdg-desktop-portal-wlr should already be enabled by hyprland
           extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+          config = {
+            # Prefer `xdg-desktop-portal-gtk` for every portal interface.
+            # For more information, see: https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in
+            common = {
+              default = [
+                "gtk"
+              ];
+            };
+          };
         };
       };
     };
