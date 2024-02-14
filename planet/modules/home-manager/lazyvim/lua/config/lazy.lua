7258905be0c463b703cc0b143012f9423655adc8
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-02-14 18:54:41 +0800

    refactor(planet/hm/lazyvim): tidy up `lazy.lua`
    Remove redundant code and clarify/add comments.

--- planet/modules/home-manager/lazyvim/lua/config/lazy.lua
+++ planet/modules/home-manager/lazyvim/lua/config/lazy.lua
@@ -6,23 +6,11 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
    spec = {
-      -- add LazyVim and import its plugins
+      -- Add LazyVim and import its plugins.
       { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-      -- import any extras modules here
-      -- { import = "lazyvim.plugins.extras.lang.typescript" },
-      -- { import = "lazyvim.plugins.extras.lang.json" },
-      -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-      -- import/override with your plugins
+
+      -- Import specs from the `plugins/` directory.
       { import = "plugins" },
-   },
-   defaults = {
-      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-      lazy = false,
-      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-      -- have outdated releases, which may break your Neovim install.
-      version = false, -- always use the latest git commit
-      -- version = "*", -- try installing the latest stable version for plugins that support semver
    },
    -- Since the plugins are managed through Nix,
    -- we don't need lazy.nvim to install or check them.
@@ -30,7 +18,9 @@ require("lazy").setup({
    checker = { enabled = false },
    performance = {
       rtp = {
-         -- disable some rtp plugins
+         -- Neovim comes with some built-in RTP plugins.
+         -- A list of these plugins can be found in Neovim's `runtime/plugin/` directory.
+         -- This option lets you disable them.
          disabled_plugins = {
             "gzip",
             -- "matchit",
