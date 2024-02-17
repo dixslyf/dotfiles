5baae0edd978d2db3822858ac0aee8201098caf8
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-02-17 12:12:29 +0800

    refactor(planet/hm/lazyvim): use plugins from Nixpkgs

--- planet/modules/home-manager/lazyvim/init.lua
+++ planet/modules/home-manager/lazyvim/init.lua
@@ -1,2 +1,7 @@
+Globals = {
+   lazy_nvim_path = "@lazy_nvim_path@",
+   plugin_dirs_lua_path = "@plugin_dirs_lua_path@",
+}
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
