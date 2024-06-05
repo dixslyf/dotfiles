985a56a8ed702a3f54a92a5088c3723e7f7c9284
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-06-06 00:04:05 +0800

    fix(planet/hm/gitui): fix theme file path

--- planet/modules/home-manager/gitui/default.nix
+++ planet/modules/home-manager/gitui/default.nix
@@ -22,7 +22,7 @@
       programs.gitui = {
         enable = true;
         keyConfig = builtins.readFile ./key_bindings.ron;
-        theme = builtins.readFile "${localFlake'.packages.catppuccin-gitui}/share/gitui/macchiato.ron";
+        theme = builtins.readFile "${localFlake'.packages.catppuccin-gitui}/share/gitui/catppuccin-macchiato.ron";
       };
     };
 }
