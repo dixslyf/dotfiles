4fdabe7bd6fcf851d583decc4b0cdf1d3fdb08f7
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-07-03 23:34:50 +0800

    fix(planet/hm/gtk): use upstream `catppuccin-papirus-folders`

--- planet/pkgs/flake-module.nix
+++ planet/pkgs/flake-module.nix
@@ -18,10 +18,6 @@
         # TODO: figure out lib.makeScope and lib.callPackageWith
         pers-pkgs = npinsPackages // {
           nvidia-offload = prev.callPackage ./nvidia-offload { };
-          catppuccin-papirus-icon-theme = prev.callPackage ./catppuccin-papirus {
-            inherit (npinsPackages) catppuccin-papirus-folders;
-            catppuccin-papirus-folders-source = sources.catppuccin-papirus-folders;
-          };
           iosevka-custom = prev.callPackage ./iosevka-custom { };
           iosevka-term-custom = prev.callPackage ./iosevka-custom { spacing = "term"; };
           # vimPlugins = prev.lib.recurseIntoAttrs (prev.callPackage ./vim-plugins { });
