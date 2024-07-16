e66ac28ad87bf46e07b20439249898837329de9d
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-07-16 21:12:58 +0800

    fix(planet/hm/lazyvim): remove workaround for plugin specs
    Bug has been fixed.

--- planet/modules/home-manager/lazyvim/lua/config/lazy.lua
+++ planet/modules/home-manager/lazyvim/lua/config/lazy.lua
@@ -2,19 +2,14 @@ local lazypath = Globals.lazy_nvim_path
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-   spec = vim.list_extend(
-      -- Override directories for plugins loaded by LazyVim.
-      vim.list_extend(dofile(Globals.plugin_dirs_lua_path), {
-         -- Add LazyVim and import its plugins.
-         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+   -- Override directories for plugins loaded by LazyVim.
+   spec = vim.list_extend(dofile(Globals.plugin_dirs_lua_path), {
+      -- Add LazyVim and import its plugins.
+      { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-         -- Import specs from the `plugins/` directory.
-         { import = "plugins" },
-      }),
-      -- Due to the bug below, `dir` gets overwritten back to the default, so we set it again.
-      -- https://github.com/folke/lazy.nvim/issues/1328
-      dofile(Globals.plugin_dirs_lua_path)
-   ),
+      -- Import specs from the `plugins/` directory.
+      { import = "plugins" },
+   }),
    -- Since the plugins are managed through Nix,
    -- we don't need lazy.nvim to install or check them.
    install = { missing = false },
