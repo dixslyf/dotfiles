86edcc5737b663a24084bd4ff27af10f292c80bb
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-08-26 12:33:57 +0800

    feat(planet/nixos/podman): init

--- planet/modules/nixos/default.nix
+++ planet/modules/nixos/default.nix
@@ -14,6 +14,7 @@ _: {
     (importModule ./nvidia { })
     (importModule ./persistence { })
     (importModule ./pipewire { })
+    ./podman
     (importModule ./sddm { })
     ./steam
     ./tlp
---
+++ planet/modules/nixos/podman/default.nix
@@ -0,0 +1,38 @@
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  options =
+    let
+      inherit (lib) mkEnableOption;
+    in
+    {
+      planet.podman = {
+        enable = mkEnableOption "planet podman";
+      };
+    };
+
+  config =
+    let
+      cfg = config.planet.podman;
+      inherit (lib) mkIf;
+    in
+    mkIf cfg.enable {
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+      };
+
+      hardware.nvidia-container-toolkit.enable = true;
+      environment.systemPackages = with pkgs; [
+        nvidia-container-toolkit
+      ];
+
+      planet.persistence = {
+        directories = [
+          "/var/lib/containers"
+        ];
+      };
+    };
+}
