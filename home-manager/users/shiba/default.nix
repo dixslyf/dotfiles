998793fcdfa7775aec28e2086f2cc7f4669b1bf9
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-01 12:46:12 +0800

    refactor(hm/shiba): `eww` with `yuckConfig` and `scssConfig`

--- home-manager/users/shiba/default.nix
+++ home-manager/users/shiba/default.nix
@@ -195,6 +195,24 @@
   programs.eww = {
     enable = true;
     systemd.enable = true;
-    configDir = ./eww-config;
+    yuckConfig = ''
+      (defwindow example
+        :monitor 0
+        :geometry (geometry :x "0%"
+                             :y "20px"
+                             :width "90%"
+                             :height "30px"
+                             :anchor "top center")
+        :stacking "fg"
+        :reserve (struts :distance "40px" :side "top")
+        :windowtype "dock"
+        :wm-ignore false
+      "example content")
+    '';
+    scssConfig = ''
+      window {
+        background: pink;
+      }
+    '';
   };
 }
