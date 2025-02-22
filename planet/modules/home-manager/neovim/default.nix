cb5d0c8ae819bf308506f1a53a71b1d9edd26468
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-22 21:55:15 +0800

    refactor(planet/hm/neovim): add treesitter grammars for typescript and tsx

--- planet/modules/home-manager/neovim/default.nix
+++ planet/modules/home-manager/neovim/default.nix
@@ -208,6 +208,8 @@
               tree-sitter-python
               tree-sitter-rust
               tree-sitter-toml
+              tree-sitter-tsx
+              tree-sitter-typescript
               tree-sitter-typst
               tree-sitter-vim
             ]
