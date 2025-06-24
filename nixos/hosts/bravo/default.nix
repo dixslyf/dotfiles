6a95737fa648038372c9983d0d5671c66df9746d
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-24 13:28:05 +0800

    refactor(nixos): switch to new cachix cache

--- nixos/hosts/bravo/default.nix
+++ nixos/hosts/bravo/default.nix
@@ -28,11 +28,11 @@
         "flakes"
       ];
       substituters = [
-        "https://playernamehere-nixos.cachix.org"
+        "https://dixslyf.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "playernamehere-nixos.cachix.org-1:oUHiXCIHbB+VbjDIlckfjwaYkJEvpkW3250+FhF5Vi4="
+        "dixslyf.cachix.org-1:6x8b4tr/2LBObGAlAGS1fbW3B3nK1FvL0CH9uRxjmI4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
