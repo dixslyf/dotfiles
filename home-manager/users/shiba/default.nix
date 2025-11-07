1f02c32dcbf8eb13de417a575ffcfb31eba18732
Dixon Sean Low Yan Feng <dixon@subnero.com>
2025-11-07 11:35:51 +0800

    refactor(hm/shiba): use devenv from overlay

--- home-manager/users/shiba/default.nix
+++ home-manager/users/shiba/default.nix
@@ -1,6 +1,5 @@
 {
   importModule,
-  localFlakeInputs',
   ...
 }:
 
@@ -179,7 +178,7 @@
     bottom
     ouch
     yazi
-    localFlakeInputs'.devenv.packages.default
+    devenv
 
     # Media
     pavucontrol
