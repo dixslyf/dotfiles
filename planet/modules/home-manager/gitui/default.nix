1357a50ab2aca095ab0225ec782f8990b9af1a89
Dixon Sean Low Yan Feng <dixon@subnero.com>
2025-11-07 10:36:38 +0800

    refactor(planet/hm/gitui): allow specifying package

--- planet/modules/home-manager/gitui/default.nix
+++ planet/modules/home-manager/gitui/default.nix
@@ -7,11 +7,20 @@
 {
   options =
     let
-      inherit (lib) mkEnableOption;
+      inherit (lib)
+        mkEnableOption
+        mkOption
+        types
+        ;
     in
     {
       planet.gitui = {
         enable = mkEnableOption "planet gitui";
+        package = mkOption {
+          type = types.package;
+          default = pkgs.gitui;
+          description = "The `gitui` package to use.";
+        };
       };
     };
 
@@ -23,6 +32,7 @@
     mkIf cfg.enable {
       programs.gitui = {
         enable = true;
+        inherit (cfg) package;
         keyConfig = builtins.readFile ./key_bindings.ron;
         theme = builtins.readFile "${pkgs.pers-pkgs.catppuccin-gitui}/share/gitui/catppuccin-macchiato.ron";
       };
