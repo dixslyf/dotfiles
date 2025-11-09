3356b28986d003579d072260580bf1383de3ec0b
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-11-09 15:14:25 +0800

    refactor(nixos/delta): set system-level configuration

--- nixos/hosts/delta/default.nix
+++ nixos/hosts/delta/default.nix
@@ -2,7 +2,10 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-_:
+{
+  pkgs,
+  ...
+}:
 
 {
   imports = [
@@ -13,109 +16,143 @@ _:
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernel = {
+      sysctl = {
+        "kernel.sysrq" = 1; # https://wiki.archlinux.org/title/Keyboard_shortcuts#Kernel_(SysRq)
+      };
+    };
+  };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      keep-outputs = true;
+      keep-derivations = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://dixslyf.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://devenv.cachix.org"
+      ];
+      trusted-public-keys = [
+        "dixslyf.cachix.org-1:6x8b4tr/2LBObGAlAGS1fbW3B3nK1FvL0CH9uRxjmI4="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+    };
+    # Garbage collection.
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+  };
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  security = {
+    sudo = {
+      execWheelOnly = true;
+      extraConfig = "Defaults lecture=never";
+    };
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Tell gc to wait for /persist to be mounted to prevent it from
+  # thinking that the gc roots are invalid. Otherwise, my devShells get gc'd.
+  systemd.services.nix-gc.unitConfig = {
+    RequiresMountsFor = "/persist";
+  };
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  networking.hostName = "delta";
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  time.timeZone = "Asia/Singapore";
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  i18n.defaultLocale = "en_SG.UTF-8";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services = {
+    btrfs.autoScrub.enable = true;
+    logind = {
+      settings = {
+        Login.HandlePowerKey = "ignore";
+      };
+    };
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+      };
+      displayManager = {
+        # Set the background color of the root window
+        sessionCommands = ''
+          ${pkgs.hsetroot}/bin/hsetroot -solid "#363a4f"
+        '';
+      };
+      windowManager.bspwm.enable = true;
+      excludePackages = with pkgs; [ xterm ];
+    };
+    displayManager.defaultSession = "none+bspwm";
+    libinput = {
+      enable = true;
+      touchpad.naturalScrolling = true;
+    };
+    fstrim.enable = true;
+    autorandr.enable = true;
+    resolved.enable = true;
+    blueman.enable = true;
+    envfs.enable = true;
+  };
 
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  planet = {
+    earlyoom.enable = true;
+    flatpak.enable = true;
+    mullvad-vpn.enable = true;
+    neovim.enable = true;
+    networkmanager.enable = true;
+    pipewire = {
+      enable = true;
+      lowLatency = true;
+    };
+    podman.enable = true;
+    qmk.enable = true;
+    sddm.enable = true;
+    tlp = {
+      enable = true;
+      diskDevices = [
+        "nvme0n1"
+      ];
+    };
+    udisks2.enable = true;
+    upower.enable = true;
+    xdg.enable = true;
+  };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  # Packages
+  nixpkgs.config.allowUnfree = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
+  fonts.packages = [ pkgs.pers-pkgs.mali ];
 
-  # programs.firefox.enable = true;
+  environment.systemPackages = with pkgs; [
+    pciutils
+    light
+    pamixer
+  ];
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  programs = {
+    light.enable = true;
+    gamemode.enable = true;
+    dconf.enable = true; # Required by home-manager if `gtk.enable = true`
+    nix-ld.enable = true;
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05"; # Do not change!
 }
