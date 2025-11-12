a6437d1d07ab08af309f1cb3b1873dd9533e02e1
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-11-12 22:09:26 +0800

    feat(hm/husky): configure other secrets

--- home-manager/users/husky/sops/default.nix
+++ home-manager/users/husky/sops/default.nix
@@ -1,4 +1,8 @@
-_: {
+{
+  config,
+  ...
+}:
+{
   planet.persistence = {
     directories = [
       ".config/sops"
@@ -7,7 +11,16 @@ _: {
 
   sops = {
     age.keyFile = "/persist/home/husky/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets.yaml;
     secrets = {
+      gh_hosts = {
+        mode = "0600";
+        path = "${config.xdg.configHome}/gh/hosts.yml";
+      };
+      glab_config = {
+        mode = "0600";
+        path = "${config.xdg.configHome}/glab-cli/config.yml";
+      };
       syncthing-gui-password = {
         sopsFile = ./syncthing/gui_password.yaml;
         mode = "0600";
