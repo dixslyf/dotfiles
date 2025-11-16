8122f5e77a8c7c1c074075e4f72ae32e9a7d45f3
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-11-16 11:11:44 +0800

    refactor(nixos/delta): disable external monitor in sddm

--- nixos/hosts/delta/default.nix
+++ nixos/hosts/delta/default.nix
@@ -109,6 +109,14 @@
         }
       ];
       displayManager = {
+        # Disable external monitor
+        setupCommands = ''
+          ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --off
+          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off
+          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --off
+          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-3 --off
+        '';
+
         # Set the background color of the root window
         sessionCommands = ''
           ${pkgs.hsetroot}/bin/hsetroot -solid "#363a4f"
