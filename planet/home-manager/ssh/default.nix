ea00ab5d0f6bf6bb53db42a6ba3c963787a6f06c
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2026-01-04 12:36:54 +0800

    fix(planet/hm/ssh): allow using `extraConfig`
    For some reason, there must be a `*` block in order to use
    `extraConfig`.

--- planet/home-manager/ssh/default.nix
+++ planet/home-manager/ssh/default.nix
@@ -23,6 +23,7 @@
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
+        matchBlocks."*" = { };
       };
       planet.persistence = {
         directories = [ ".ssh" ];
