5c6083289a86a339be1dda4b3f56cba707afc9c5
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2026-08-16 12:42:36 +0800

    fix(planet/hm/claude-code): work around non-compliance with xdg base directory spec

--- planet/home-manager/claude-code/default.nix
+++ planet/home-manager/claude-code/default.nix
@@ -21,13 +21,31 @@
       inherit (lib) mkIf;
     in
     mkIf cfg.enable {
-      home.packages = with pkgs; [
-        claude-code
-      ];
+      home = {
+        packages = with pkgs; [
+          claude-code
+        ];
+
+        # https://github.com/anthropics/claude-code/issues/1455
+        sessionVariables = {
+          CLAUDE_CONFIG_DIR = "${config.home.homeDirectory}/.config/claude";
+        };
+
+        file = {
+          # Annoying, but this doesn't live in the config dir, so
+          # we'll manually symlink.
+          ".claude.json".source =
+            config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.CLAUDE_CONFIG_DIR}/.claude.json";
+        };
+      };
 
       planet.persistence = {
         directories = [
+          # We still need to persist ~/.claude because claude-code still uses it for some things
+          # even after setting CLAUDE_CONFIG_DIR.
+          # https://github.com/anthropics/claude-code/issues/1455
           ".claude"
+          ".config/claude"
         ];
       };
     };
